7f6e098c5296cd1222da9a8599da23ab40085112
negiFSD <sunilnegi217@rediffmail.com>
2022-06-08 18:16:22 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,77 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
+import FormInput from "./Components/FormInput";
 
 function App() {
+  const [values, setValues] = useState({
+    userName: "",
+    email: "",
+    birthday: "",
+    password: "",
+    confirmPassword: "",
+  });
+  //  console.log(userName)
+  const inputs = [
+    {
+      id: 1,
+      name: "userName",
+      type: "text",
+      placeholder: "userName",
+      label: "userName",
+    },
+    {
+      id: 2,
+      name: "email",
+      type: "text",
+      placeholder: "Email",
+      label: "Email",
+    },
+    {
+      id: 3,
+      name: "birthday",
+      type: "text",
+      placeholder: "Birthday",
+      label: "Birthday",
+    },
+    {
+      id: 4,
+      name: "password",
+      type: "text",
+      placeholder: "Password",
+      label: "Password",
+    },
+    {
+      id: 5,
+      name: "confirmPassword",
+      type: "text",
+      placeholder: "ConfirmPassword",
+      label: "ConfirmPassword",
+    },
+  ];
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  };
+  const onChange = (e) => {
+    console.log(e);
+    setValues({ ...values, [e.target.name]: e.target.value });
+  };
+  console.log(values);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <form onSubmit={handleSubmit}>
+
+        {inputs.map((input) => (
+          <FormInput
+            key={input.id}
+            {...input}
+            value={values[input.name]}
+            onChange={onChange}
+          />
+        ))}
+
+        <button type="submit">Submit</button>
+      </form>
     </div>
   );
 }
